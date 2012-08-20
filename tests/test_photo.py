0d7e89847958b9be7c7537e9c0017a04aecf497d
Zach Aysan <zachaysan@gmail.com>
2012-08-20 18:22:30 -0400

    make the initial photo tests

---
+++ tests/test_photo.py
@@ -0,0 +1,33 @@
+import unittest
+from models.photo import Photo
+
+class Test_retrieve_photo(unittest.TestCase):
+    def setUp(self):
+        self.owly_photo = Photo(3256058)
+
+    def test_init(self):
+        self.assertEqual(self.owly_photo.id, 3256058)
+    
+    def test_nsfw(self):
+        self.assertFalse(self.owly_photo.nsfw)
+        
+    def test_status(self):
+        self.assertEqual(self.owly_photo.status, 1)
+        
+    def test_status_name(self):
+        self.assertEqual(self.owly_photo.status_name, 'status_active')
+
+    def test_photos_user_cache(self):
+        owly_photo = Photo(3256058)
+        same_owly_photo = Photo(3256058)
+        self.assertEqual(owly_photo.__hash__(), same_owly_photo.__hash__())
+
+        different_owly_photo = Photo(4600083)
+        self.assertNotEqual(owly_photo.__hash__(), different_owly_photo.__hash__())
+        
+    def test_dir_for_magic_user_generation(self):
+        self.assertIn('user', dir(self.owly_photo))
+        
+    def test_photo_autogens_user(self):
+        user = self.owly_photo.user
+        self.assertEqual(user.username, 'AlexThomson')
