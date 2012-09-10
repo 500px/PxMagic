af537ccdee15f97aebed4070f4ee972ec963d3a8
Zach Aysan <zachaysan@gmail.com>
2012-09-10 11:13:28 -0400

    add user search model

---
+++ fhp/models/user_search.py
@@ -0,0 +1,25 @@
+from functools import partial
+
+from fhp.src import fivehundred
+from fhp.helpers import authentication
+
+import fhp.models.user
+
+from fhp.magic.magic_cache import magic_fn_cache
+from fhp.magic.magic_generator import MagicGenerator
+
+five_hundred_px = fivehundred.FiveHundredPx(authentication.get_consumer_key(),
+                                            authentication.get_consumer_secret())
+@magic_fn_cache
+def UserSearch(term):
+    iter_source = partial(five_hundred_px.user_search, term=term)
+    def build_user(user_data):
+        user_id = user_data["id"]
+        data = dict(user=user_data)
+        return fhp.models.user.User(user_id, data=data)
+    iter_destination = build_user
+    return user_search(iter_source=iter_source,
+                       iter_destination=iter_destination)
+
+class user_search(MagicGenerator):
+    pass
