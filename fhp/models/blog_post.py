bb9633143a112b8784d5242e9acfd6a4e0ac7573
Zach Aysan <zachaysan@gmail.com>
2012-09-09 21:19:17 -0400

    add comments to blog post

--- fhp/models/blog_post.py
+++ fhp/models/blog_post.py
@@ -1,11 +1,15 @@
+from functools import partial
+
 from fhp.src import fivehundred
 from fhp.helpers import authentication
 
 import fhp.models.photo
 import fhp.models.user
+import fhp.models.blog_post_comment
 
 from fhp.magic.magic_cache import magic_cache, magic_fn_cache
 from fhp.magic.magic_object import magic_object
+from fhp.magic.magic_generator import MagicGenerator
 
 @magic_fn_cache
 def BlogPost(id, *args, **kwargs):
@@ -28,7 +32,7 @@ class blog_post(magic_object):
         elif 'user' in blog_post_data:
             self.user_id = blog_post_data['user']['id']
             del(blog_post_data['user'])
-
+        
         if 'photos' in blog_post_data:
             self._make_photos_(blog_post_data['photos'])
             del(blog_post_data['photos'])
@@ -44,17 +48,32 @@ class blog_post(magic_object):
     def __getattr__(self, name):
         if name == 'user':
             return self._get_user_(self.user_id)
-        if name == 'photos':
+        elif name == 'photos':
             data = self.get_long_public_blog_post_data(self.id)
             photo_data = data.get('photos', {})
             self._make_photos_(photo_data)
             return self.photos
+        elif name == 'comments':
+            self._get_comments_()
+            return self.comments
         else:
             raise AttributeError
 
     @magic_cache
     def _get_user_(self, user_id):
         return fhp.models.user.User(self.user_id)
+
+    def _get_comments_(self):
+        iter_source = partial(blog_post.five_hundred_px.get_blog_post_comments,
+                              self.id)
+        def build_comment(blog_post_id, comment_data):
+            return fhp.models.blog_post_comment.BlogPostComment(comment_data["id"],
+                                                                blog_post_id,
+                                                                comment_data)
+        iter_destination = partial(build_comment, self.id)
+        self.comments = MagicGenerator(iter_source=iter_source,
+                                       iter_destination=iter_destination)
+
                 
     def _make_photos_(self, photo_data):
         self.photos = {}
