43b9ce8cbbf804cce5ed9a65fcd19ff29230f0ee
Zach Aysan <zachaysan@gmail.com>
2012-11-15 10:23:30 -0500

    Add warning when authentication has not been complete

--- fhp/helpers/__init__.py
+++ fhp/helpers/__init__.py
@@ -1,3 +1,14 @@
-import authentication
+try:
+    import authentication
+except ImportError:
+    import sys
+    warn_user = """No authentication set
+
+
+*** You must set your authentication in fhp.helpers.authentication in order
+ to use PxMagic. Edit the example file and save as authentication.py ***
+
+"""
+    sys.exit(warn_user)
 import http
 import json_finder
